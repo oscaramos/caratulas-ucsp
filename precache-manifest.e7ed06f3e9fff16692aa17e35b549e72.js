982a1a6347a09918c62a6558c597a278a1506553
oscaramos <oscar.ramos@ucsp.edu.pe>
2020-06-11 23:03:54 +0000

    Deploying to gh-pages from  @ 6e5b4c383e82b7887426fe072a222b9601237a4c 🚀

---
+++ precache-manifest.e7ed06f3e9fff16692aa17e35b549e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefc068edb4990b5c11dcbe4e91d9a88",
+    "url": "/caratulas-ucsp/index.html"
+  },
+  {
+    "revision": "78283225c9ccc06032a5",
+    "url": "/caratulas-ucsp/static/js/2.851c1176.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/caratulas-ucsp/static/js/2.851c1176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b60eedea3f4a8ef6503",
+    "url": "/caratulas-ucsp/static/js/main.1fdc0f96.chunk.js"
+  },
+  {
+    "revision": "0b5561329d92f12542c4",
+    "url": "/caratulas-ucsp/static/js/runtime-main.1f545457.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caratulas-ucsp/precache-manifest.0e7eb8af5cf1aabbd14c0ec193095bb1.js"
+  "/caratulas-ucsp/precache-manifest.e7ed06f3e9fff16692aa17e35b549e72.js"
 );
 
 self.addEventListener('message', (event) => {
