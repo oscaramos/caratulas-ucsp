da82875a22d46bdb3ed5366cf4da16a34436ae42
oscaramos <oscar.ramos@ucsp.edu.pe>
2020-08-07 00:08:13 +0000

    Deploying to gh-pages from  @ 715d710fdcd6e81d669572427e9bce14244e3b6e 🚀

---
+++ precache-manifest.d77a6afe08ac0e5562797f36c5c3753d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a31e6f8c9b24af032e133425eb8fec",
+    "url": "/caratulas-ucsp/index.html"
+  },
+  {
+    "revision": "e119931f28114a5e4693",
+    "url": "/caratulas-ucsp/static/js/2.64ebd023.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/caratulas-ucsp/static/js/2.64ebd023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da9af75a5b877cdf19f",
+    "url": "/caratulas-ucsp/static/js/main.974f2c83.chunk.js"
+  },
+  {
+    "revision": "0b5561329d92f12542c4",
+    "url": "/caratulas-ucsp/static/js/runtime-main.1f545457.js"
+  }
+]);
