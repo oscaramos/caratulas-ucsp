425d828d8fe89c5048984e6774210ef2643f3e2b
oscaramos <odrr.99@gmail.com>
2022-04-30 17:46:19 +0000

    Deploying to gh-pages from  @ 00e84aea0ce8c1eb2b57e723bed89864fd740f66 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caratulas-ucsp/precache-manifest.2395292447dc3c44a09bb8dd2d697ad4.js"
+  "/caratulas-ucsp/precache-manifest.1621a58c9a9b6bd937c356c08eeee658.js"
 );
 
 self.addEventListener('message', (event) => {
